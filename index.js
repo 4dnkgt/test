fb3cdfdb4e658de77cb2e031c8a46235c32e10f4
4dnkgt <68125076+4dnkgt@users.noreply.github.com>
2020-10-22 08:10:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
