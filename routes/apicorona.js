682320e8c8f7fa1b97874aeb5af00e2db7529fad
4dnkgt <68125076+4dnkgt@users.noreply.github.com>
2020-10-22 08:54:46 -0700

    Update apicorona.js

--- routes/apicorona.js
+++ routes/apicorona.js
@@ -15,18 +15,18 @@ router.get('/corona', (req, res) => {
 router.get('/corona/continent=:continent', async (req, res) => {
     const body = await fetch(`https://disease.sh/v3/covid-19/continents/${req.params.continent}?yesterday=true&twoDaysAgo=false&strict=true&allowNull=false`).then(response => response.json());
     try {
-      const capispecfifc = [
-          {cases: `${body.cases}`}, 
-          {casesToday: `${body.todayCases}`},
-          {population: `${body.population}`},
-          {deaths: `${body.deaths}`},
-          {recovered: `${body.recovered}`},
-          {recoveredToday: `${body.todayRecovered}`},
-          {active: `${body.active}`},
-          {criticalPerOneMillion: `${body.criticalPerOneMillion}`},
-          {casesPerOneMillion: `${body.casesPerOneMillion}`},
-          {critical: `${body.critical}`}
-      ]
+      const capispecfifc = {
+          cases: `${body.cases}`, 
+          casesToday: `${body.todayCases}`,
+          population: `${body.population}`,
+          deaths: `${body.deaths}`,
+          recovered: `${body.recovered}`,
+          recoveredToday: `${body.todayRecovered}`,
+          active: `${body.active}`,
+          criticalPerOneMillion: `${body.criticalPerOneMillion}`,
+          casesPerOneMillion: `${body.casesPerOneMillion}`,
+          critical: `${body.critical}`
+    }
       res.json(capispecfifc)
     } catch(err) {
         const errormessage = [
@@ -38,11 +38,73 @@ router.get('/corona/continent=:continent', async (req, res) => {
 
 router.get('/corona/all', async (req, res) => {
     const body = await fetch('https://disease.sh/v3/covid-19/all?yesterday=true&twoDaysAgo=false&allowNull=true').then(response => response.json());
-    const capi = [
-        {cases: `${body.cases}`}, 
-        {casesToday: `${body.todayCases}`},
-        {population: `${body.population}`},
-        {flag: `${body.flag}`},
+    const capi = {
+        cases: `${body.cases}`, 
+        casesToday: `${body.todayCases}`,
+        population: `${body.population}`,
+        deaths: `${body.deaths}`,
+        recovered: `${body.recovered}`,
+        recoveredToday: `${body.todayRecovered}`,
+        active: `${body.active}`,
+        critical: `${body.critical}`
+}
+    res.json(capi)
+})
+
+
+router.get('/corona/state=:states', async (req, res) => {
+    const data = encodeURI(`https://disease.sh/v3/covid-19/states/${req.params.states}?yesterday=true&allowNull=false`)
+    const body = await fetch(`${data}`).then(response => response.json());
+    try {
+      const capispecfifc = {
+          cases: `${body.cases}`, 
+          casesToday: `${body.todayCases}`,
+          population: `${body.population}`,
+          deaths: `${body.deaths}`,
+          recovered: `${body.recovered}`,
+          active: `${body.active}`,
+          tests: `${body.tests}`,
+          testsPerMillion: `${body.testsPerOneMillion}`,
+          casesPerOneMillion: `${body.casesPerOneMillion}`
+      }
+      res.json(capispecfifc)
+    } catch(err) {
+        const errormessage = [
+            {message: `${err}`}
+        ]
+        res.send(errormessage)
+    }  
+  })
+
+router.get('/corona/country=:country', async (req, res) => {
+  const body = await fetch(`https://disease.sh/v3/covid-19/countries/${req.params.country}?yesterday=true&twoDaysAgo=false&strict=false&allowNull=false`).then(response => response.json());
+  try {
+    const capispecfifc = {
+        cases: `${body.cases}`, 
+        casesToday: `${body.todayCases}`,
+        population: `${body.population}`,
+        deaths: `${body.deaths}`,
+        recovered: `${body.recovered}`,
+        recoveredToday: `${body.todayRecovered}`,
+        active: `${body.active}`,
+        oneCasePerPerson: `${body.oneCasePerPeople}`,
+        oneDeathPerPerson: `${body.oneDeathPerPeople}`,
+        criticalPerOneMillion: `${body.criticalPerOneMillion}`,
+        casesPerOneMillion: `${body.casesPerOneMillion}`,
+        critical: `${body.critical}`
+    }
+    res.json(capispecfifc)
+  } catch(err) {
+      const errormessage = [
+          {message: `${err}`}
+      ]
+      res.send(errormessage)
+  }
+
+})
+
+
+module.exports = router;
         {deaths: `${body.deaths}`},
         {recovered: `${body.recovered}`},
         {recoveredToday: `${body.todayRecovered}`},
